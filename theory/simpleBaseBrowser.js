536325795841174fe404fd77c725d40bc2206d51
temporaryz <74442347+Shizofronik@users.noreply.github.com>
2025-04-16 16:06:04 +0700

    addition DOM(navigation)
    Навигация в DOM-дереве, различие узлов от обычных элементов

--- theory/simpleBaseBrowser.js
+++ theory/simpleBaseBrowser.js
@@ -21,3 +21,39 @@ console.log('parse:', JSON.parse(window.localStorage.getItem('id')))
 //⁡⁣⁣⁢DOM(Document Object Model)⁡ DOM-дерево
 // объектная модель документа для взаимодействия с содержимым страницы в виде объектов
 console.log(window.document)//HTML код страницы в обертке
+
+console.log(document.documentElement)
+console.log(document.head)
+console.log(document.body)//будет null, если скрипт подключен не в body
+
+console.log('Родительский элемент body:', document.body.parentElement)
+console.log('Соседний элемент перед body:', document.body.previousElementSibling)
+console.log('Соседний элемент после body:', document.body.nextElementSibling)//null
+
+console.log('Дочерние элементы body:', document.body.children)//Это не список или объект, а HTMLCollection
+//HTMLCollection всегда отражает актуальное состояние DOM-дерева при обращении к HTMLCollection
+
+//Через spread оператор преобразуем HTMLCollection в массив и переберем еe
+const childrens = [...document.body.children]
+
+childrens.forEach((child) => {
+    console.log('Дочерний элемент body:', child)
+})
+
+
+console.log('Первый дочерний элемент body:',document.body.firstElementChild)
+
+
+//⁡⁣⁣⁡⁣⁣⁢NODE(узлы)⁡
+
+//parentNode в данном случае будет ссылаться на сам document, в остальном его поведение будет таким же
+console.log('html parent:', document.documentElement.parentElement)//null
+console.log('html parent:', document.documentElement.parentNode)
+
+console.log('previous:', document.body.firstElementChild.previousElementSibling)//null
+//А в этом слкчае будет ссылаться на текстовый узел "\n   "(перенос строки в самом коде)
+console.log('previous:', document.body.firstElementChild.previousSibling)
+
+//firstChild и lastChild тоже возвращают не эелементы, а текстовые узлы
+console.log('first children node:', document.body.firstChild)
+console.log('last children node:', document.body.lastChild)
